acda907b147fadf91f08a9b6d5dbcde55d053c69
mariContado <your-email@example.com>
2025-11-30 18:47:53 +0800

    Initial commit

---
+++ modules/Paris.php
@@ -0,0 +1,8 @@
+
+<h1>Paris</h1>
+<img src="assets/img/paris.jpg" class="img-fluid">
+<p>Paris is the capital city of France. It is one of the most visited and culturally influential cities in the world, known for its art, fashion, gastronomy, and iconic landmarks.</p>
+<p>Located along the River Seine, Paris has a rich history dating back to ancient times. Originally settled by the Parisii tribe, the city later flourished under Roman rule and eventually became the political and cultural heart of France.</p>
+
+
+
---
+++ modules/Tokyo.php
@@ -0,0 +1,7 @@
+
+<h1>Tokyo</h1>
+<img src="assets/img/tokyo.jpg" class="img-fluid">
+<p>Tokyo is the capital city of Japan and one of the largest metropolitan areas in the world. Known for its blend of modern technology, vibrant culture, and rich history, Tokyo is a global center for finance, innovation, and entertainment.</p>
+<p>Originally a small fishing village called Edo, the city grew rapidly after becoming the seat of the Tokugawa shogunate. It was renamed Tokyo, meaning "Eastern Capital," in 1868 when it became the capital of Japan.</p>
+
+
